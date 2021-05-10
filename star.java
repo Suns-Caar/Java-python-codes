fe20ee1bedf7a74a4ec23524a7a29dbd4437d679
Sanskar Chandaliya <73470491+Suns-Caar@users.noreply.github.com>
2021-05-10 17:40:32 +0530

    Update star.java

--- star.java
+++ star.java
@@ -7,9 +7,12 @@
 import java.util.Scanner;
 public class inputfromtheuser {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of rows ");
+        int x =input.nextInt();
 
         int i = 0; int j = 0;
-        for(i = 0;i <= 4;i++)
+        for(i = 0;i < x;i++)
         { for (j = 0;j <= i;j++)
         {
             System.out.print("*");
