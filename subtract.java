8e41efdad6c0bcfb49292dca52d38174f5632741
Sanskar Chandaliya <73470491+Suns-Caar@users.noreply.github.com>
2021-05-10 17:54:54 +0530

    Create subtract.java

---
+++ subtract.java
@@ -0,0 +1,14 @@
+//
+import java.util.Scanner;
+public class subtract{
+public static void main(String args[]){
+int sub;
+Scanner scan=new Scanner(System.in);
+
+System.out.print("Enter the value to num1: ");
+int num1=scan.nextInt();
+System.out.print("Enter the value to num2: ");
+ int num2 = scan.nextInt();
+System.out.print(Math.subtractExact(num1,num2))
+    }
+}
