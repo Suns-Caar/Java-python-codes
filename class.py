4cf24f4e1934872a5818e06ba1cb35a3d8a4c436
Sanskar Chandaliya <73470491+Suns-Caar@users.noreply.github.com>
2021-05-15 23:55:07 +0530

    Create class.py

---
+++ class.py
@@ -0,0 +1,14 @@
+class hello_world:
+    def __init__(self, first):
+        self.first = first
+hello = hello_world('Hello World')
+print(hello.first)
+
+
+
+class intro:
+    def __init__(self, introduction):
+        self.introduction = introduction
+Intro = intro('Hi I am a student of MIT')
+print(Intro.introduction)
+© 2021 GitHub,
