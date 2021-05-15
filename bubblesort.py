b3e74b0002bdfcd2a5ecd2b3f96bd9d1c23d6ba1
Sanskar Chandaliya <73470491+Suns-Caar@users.noreply.github.com>
2021-05-15 22:01:26 +0530

    Create bubblesort.py

---
+++ bubblesort.py
@@ -0,0 +1,15 @@
+#Create an integer array, fill it with user’s inputs and then sort it with bubble sort
+def bubblesort(arr):
+
+    for i in range(0,len(arr)):
+        for j in range(1,len(arr)):
+            if(arr[j] > arr[j-1]):
+                arr[j],arr[j-1] = arr[j-1], arr[j]
+    return arr
+arr = []
+
+for i in range(0,3):
+    x = int(input('enter the numbers: '))
+    arr.append(x)
+print("unsorted array is:",arr)
+print("the sorted arr: ", bubblesort(arr))
