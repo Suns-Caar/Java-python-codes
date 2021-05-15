1297b647e505ca26fb258d1523362b18acfb7b5e
Sanskar Chandaliya <73470491+Suns-Caar@users.noreply.github.com>
2021-05-15 22:39:41 +0530

    Create input from user.py

---
+++ input from user.py
@@ -0,0 +1,10 @@
+#input from user
+str = input("Enter string: ")
+integer = input("Enter an integer: ")
+char = input("Enter a character: ")
+dub = input("Enter a decimal number: ")
+
+print("The string is ", string)
+print("The integer is ", integer)
+print("The character is ", character)
+print("The double is ", double)
