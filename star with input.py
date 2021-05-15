851768168e62a1e739bf8152d87036c6474d0647
Sanskar Chandaliya <73470491+Suns-Caar@users.noreply.github.com>
2021-05-15 22:40:36 +0530

    Create star with input.py

---
+++ star with input.py
@@ -0,0 +1,7 @@
+# print star pattern with input
+n = int(input("Enter the number of rows"))  
+for i in range(0, n):  
+        for j in range(0, i + 1): 
+            print("* ", end="")       
+        print()  
+© 2021 
