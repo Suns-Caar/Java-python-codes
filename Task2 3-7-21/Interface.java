3900037fdbe78ebdf728b1aa6686a1ba437af953
Sanskar Chandaliya <73470491+Suns-Caar@users.noreply.github.com>
2021-07-03 23:41:33 +0530

    Create Interface.java

---
+++ Task2 3-7-21/Interface.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+interface Temperature {
+    public float temp(float t);
+}
+ class FahrenheitCelsius implements Temperature {
+
+    @Override
+    public float temp(float t) {
+        t = ((t - 32) * 5) / 9;
+        return t;
+    }
+}
+
+public class Interface {
+    public static void main(String[] args) {
+        System.out.println("Enter temperature in Fahrenheit");
+        Scanner scan = new Scanner(System.in);
+        float t = scan.nextFloat();
+
+        Temperature conver = new FahrenheitCelsius();
+        float celsius = conver.temp(t);
+        System.out.println("Temp in celsius is:" + celsius);
+    }
+}
